592d716c573b1ab140aba320eb7c73a11f0a7ab2
Mansub Hafeez <mansubsheikh4@gmail.com>
2025-05-11 22:32:08 +0500

    Added Logo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
 export default function Footer() {
   return (
     <footer className="text-center text-gray-400 py-8 border-t border-gray-800 mt-16">
-      <p>© {new Date().getFullYear()} Najam. All rights reserved.</p>
+      <p>© {new Date().getFullYear()} Mansub. All rights reserved.</p>
     </footer>
   );
 }
